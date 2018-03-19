3de9eec6c2b114631758f1f1a2c84a99ef647cd3
Yilin Liu <yilin2548@gmail.com>
2018-03-19 13:50:04 +0000

    Added starters for LabW09

---
+++ W09/LabW09 - Starter/rational-test.cpp
@@ -0,0 +1,20 @@
+#include "gtest/gtest.h"
+
+#include "Rational.h"
+
+// Uncomment the todouble.1 test to run it
+// TEST(todouble, 1){
+//   Rational a;
+//   double b = (double)a + 5.6;
+//   EXPECT_DOUBLE_EQ(b, 6.6);
+// }
+
+// add at least 3 tests here to show your conversion works
+
+// Uncomment the staticCount.1 test to run it
+// TEST(staticCount, 1){
+//   EXPECT_EQ(Rational::numRationals(), 0);
+// }
+
+// add at least 3 tests here to show your static variable
+// and function work as specified
