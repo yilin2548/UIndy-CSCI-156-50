e1fa02476015eb196bcf113dd2dcdd69265d5e13
Yilin Liu <yilin2548@gmail.com>
2018-03-28 17:37:51 +0000

    Added solution for Lab W08

---
+++ Solutions/LabW08/LinkedList.h
@@ -0,0 +1,36 @@
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+/* Name: Yilin Liu-Leitke
+   Date: 3/3/2018
+   Desc: Definition of LinkedList class
+*/
+// You don't have to change this file
+#include <string>
+
+using namespace std;
+struct Node{
+    int value;
+    Node *next;
+};
+class LinkedList{
+  private:
+    Node *head;
+    Node *tail;
+    
+  public:
+    // Constructor
+    LinkedList();
+    
+    // Destructor
+    ~LinkedList();
+    
+    // Operations
+    void appendNode(int);
+    void deleteNode(int);
+    string getListAsString() const;
+    
+    // Lab W08
+    int countNode();
+    bool containNode(int);
+};
+#endif
