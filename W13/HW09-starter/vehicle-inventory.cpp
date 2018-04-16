6ca5d3193c313faa87204f48a252a77b6faf88c6
Yilin Liu <yilin2548@gmail.com>
2018-04-16 19:08:46 +0000

    Added starter for HW09

---
+++ W13/HW09-starter/vehicle-inventory.cpp
@@ -0,0 +1,15 @@
+/*  Name: <fill me in>
+    Date: <fill me in>
+    Description: main class for vehicle program
+*/
+#include <iostream>
+#include <vector>
+#include <string>
+#include "vehicle.h"
+
+using namespace std;
+
+int main(){
+    
+    return 0;
+}
